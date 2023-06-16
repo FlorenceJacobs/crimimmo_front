555ab7766b95339778dd1efb83a60bce2e21c0f0
Florence Jacobs <florencejacobs85@gmail.com>
2023-06-16 16:55:42 +0200

    clear-inutils

--- src/app/features/owner/owner.component.ts
+++ src/app/features/owner/owner.component.ts
@@ -1,40 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem, MessageService } from 'primeng/api';
 import { UserModel } from '../../shared/models/user.model';
 
 @Component({
   templateUrl: './owner.component.html',
   styleUrls: ['./owner.component.scss']
 })
-export class OwnerComponent implements OnInit {
-    itemsLogout!: MenuItem[];
-    user!: UserModel;
+export class OwnerComponent {
 
-
-    constructor(private messageService: MessageService) {}
+    constructor() {}
     
-    ngOnInit() {
-        this.itemsLogout = [
-            {
-                items: [
-                    {
-                        label: 'My rentals',
-                        icon: 'pi pi-list',
-                        routerLink: '/fileupload'
-                    },
-                    {
-                        label: 'Add a rental',
-                        icon: 'pi pi-home',
-                        routerLink: '/fileupload'
-                    },
-                    {
-                        label: 'Log OUT',
-                        icon: 'pi sign-out',
-                        routerLink: '/fileupload'
-                    }
-                ]
-            }
-        ];
-    }
+
 
 }
